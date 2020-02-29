8a10de849c62440408a0c42614d7b62519c193fb
Sanket Patidar <58832751+sanketpatidar@users.noreply.github.com>
2020-02-29 14:01:45 -0500

    Update unitTesting.py

--- unitTesting.py
+++ unitTesting.py
@@ -1,18 +1,31 @@
 import unittest
 import project04
 
+
 class functionTest(unittest.TestCase):
     def test_userstory03(self):
         result03 = ['I4 : Robb /Stark/ has a ERRORNEOUS Birthdate with respect to Deathdate',
                     'I6 : Jon /Barathaon/ has a ERRORNEOUS Birthdate with respect to Deathdate',
                     'I9 : Raegar /Targarayen/ has a ERRORNEOUS Birthdate with respect to Deathdate',
                     'I12 : Margery /Tyrell/ has a ERRORNEOUS Birthdate with respect to Deathdate']
-        self.assertEqual(project04.us03(),result03) 
+        self.assertEqual(project04.us03(), result03)
 
     def test_userstory04(self):
         result04 = ['F2 : Robert /Barathaon/ and Cate /Laniaster/ have a ERRORNEOUS Marriage date with respect to Divorced date',
                     'F5 : Olenna /Tully/ and Ramsay /Tyrell/ have a ERRORNEOUS Marriage date with respect to Divorced date']
-        self.assertEqual(project04.us04(),result04) 
+        self.assertEqual(project04.us04(), result04)
 
-if __name__=='__main__':
+    def test_userstory07(self):
+        result07 = ['I5: Robert /Barathaon/ is 154 years old which is more then 150',
+                    'I11: Olenna /Tully/ is 302 years old which is more then 150']
+        self.assertEqual(project04.US07(), result07)
+
+    def test_userstory08(self):
+        result08 = ['I6: Jon /Barathaon/ is born after 9 months from divorce of parents',
+                    'I3: Cate /Laniaster/ is born before marriage of parents',
+                    'I12: Margery /Tyrell/ is born after 9 months from divorce of parents']
+        self.assertEqual(project04.US08(), result08)
+
+
+if __name__ == '__main__':
     unittest.main()
