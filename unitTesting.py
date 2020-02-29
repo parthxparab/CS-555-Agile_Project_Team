f076cd0f99ad8fc133eadd768ac86d8f45acc1a5
Parth Parab <parthpparab@gmail.com>
2020-02-29 14:05:44 -0500

    Update unitTesting.py

--- unitTesting.py
+++ unitTesting.py
@@ -8,12 +8,19 @@ class functionTest(unittest.TestCase):
                     'I6 : Jon /Barathaon/ has a ERRORNEOUS Birthdate with respect to Deathdate',
                     'I9 : Raegar /Targarayen/ has a ERRORNEOUS Birthdate with respect to Deathdate',
                     'I12 : Margery /Tyrell/ has a ERRORNEOUS Birthdate with respect to Deathdate']
-        self.assertEqual(project04.us03(), result03)
+        self.assertEqual(project04.us03(),result03) 
 
     def test_userstory04(self):
-        result04 = ['F2 : Robert /Barathaon/ and Cate /Laniaster/ have a ERRORNEOUS Marriage date with respect to Divorced date',
-                    'F5 : Olenna /Tully/ and Ramsay /Tyrell/ have a ERRORNEOUS Marriage date with respect to Divorced date']
-        self.assertEqual(project04.us04(), result04)
+        result04 = ['F2 : Robert /Barathaon/ and Cate /Laniaster/ have a ERRORNEOUS Marriage date with respect to Divorced date', 'F3 : Jamie /Lanaster/ and Cercie /Tyrell/ have a ERRORNEOUS Marriage date with respect to Divorced date']
+        self.assertEqual(project04.us04(),result04) 
+
+    def test_userstory01(self):
+        result01 = 'Number of Entries found: 3'
+        self.assertEqual(project04.US01(),result01) 
+
+    def test_userstory02(self):
+        result02 = 'Number of Entries found: 7'
+        self.assertEqual(project04.US02(),result02) 
 
     def test_userstory07(self):
         result07 = ['I5: Robert /Barathaon/ is 154 years old which is more then 150',
