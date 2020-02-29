736c8a4330ef07f405252b992745ed395d642d84
Parth Parab <parthpparab@gmail.com>
2020-02-29 13:59:38 -0500

    added US01 & US02 and fixed  US04

--- unitTesting.py
+++ unitTesting.py
@@ -10,9 +10,17 @@ class functionTest(unittest.TestCase):
         self.assertEqual(project04.us03(),result03) 
 
     def test_userstory04(self):
-        result04 = ['F2 : Robert /Barathaon/ and Cate /Laniaster/ have a ERRORNEOUS Marriage date with respect to Divorced date',
-                    'F5 : Olenna /Tully/ and Ramsay /Tyrell/ have a ERRORNEOUS Marriage date with respect to Divorced date']
+        result04 = ['F2 : Robert /Barathaon/ and Cate /Laniaster/ have a ERRORNEOUS Marriage date with respect to Divorced date', 'F3 : Jamie /Lanaster/ and Cercie /Tyrell/ have a ERRORNEOUS Marriage date with respect to Divorced date']
         self.assertEqual(project04.us04(),result04) 
+
+    def test_userstory01(self):
+        result01 = 'Number of Entries found: 3'
+        self.assertEqual(project04.US01(),result01) 
+
+    def test_userstory02(self):
+        result02 = 'Number of Entries found: 7'
+        self.assertEqual(project04.US02(),result02) 
+
 
 if __name__=='__main__':
     unittest.main()
