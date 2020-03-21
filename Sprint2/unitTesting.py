54f0a25cc9ca8a61e4d3fc85e6f556b8d6f2fede
Vedadnya Jadhav <vedadnya@gmail.com>
2020-03-20 23:42:18 -0400

    updated testing

--- Sprint2/unitTesting.py
+++ Sprint2/unitTesting.py
@@ -4,9 +4,9 @@ import parser
 
 class functionTest(unittest.TestCase):
     def test_userstory03(self):
-        result03 = ['ERROR: INDIVIDUAL: US03: 1:  I2: Robb /Stark/ has a future Birthdate: 1960-04-04 with respect to Deathdate: 1950-09-14',
-                    'ERROR: INDIVIDUAL: US03: 5:  I6: Jon /Barathaon/ has a future Birthdate: 2050-07-10 with respect to Deathdate: 2020-03-19', 
-                    'ERROR: INDIVIDUAL: US03: 8:  I9: Raegar /Targarayen/ has a future Birthdate: 1940-07-08 with respect to Deathdate: 1920-09-08',
+        result03 = ['ERROR: INDIVIDUAL: US03: 1:  I2: Robb /Stark/ has a future Birthdate: 1960-04-04 with respect to Deathdate: 1950-09-14', 
+                    'ERROR: INDIVIDUAL: US03: 5:  I6: Jon /Barathaon/ has a future Birthdate: 2050-07-10 with respect to Deathdate: 2020-03-20', 
+                    'ERROR: INDIVIDUAL: US03: 8:  I9: Raegar /Targarayen/ has a future Birthdate: 1940-07-08 with respect to Deathdate: 1920-09-08', 
                     'ERROR: INDIVIDUAL: US03: 11:  I12: Margery /Tyrell/ has a future Birthdate: 1967-07-09 with respect to Deathdate: 1966-01-08']
         self.assertEqual(parser.us03(), result03)
 
